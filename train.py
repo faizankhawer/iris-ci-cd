ac172ebc390224e496ba01632be6a6a4e44f514e
Mohd Faizan Khawar <48196243+faizankhawer@users.noreply.github.com>
2026-04-27 20:16:10 +0530

    Update train.py

--- train.py
+++ train.py
@@ -12,8 +12,12 @@ from mlflow.models.signature import infer_signature
 
 from azure.ai.ml import MLClient
 from azure.identity import DefaultAzureCredential
-
-ml_client = MLClient.from_config(DefaultAzureCredential())
+ml_client = MLClient(
+    credential=DefaultAzureCredential(),
+    subscription_id=os.environ["AZURE_SUBSCRIPTION_ID"],
+    resource_group_name=os.environ["AZURE_RESOURCE_GROUP"],
+    workspace_name=os.environ["AZURE_WORKSPACE_NAME"],
+)
 def validate_env():
     required_vars = [
         "AZURE_SUBSCRIPTION_ID",
